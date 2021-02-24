4ae0a4400703b1aace513f39c5559dd588a21ae1
dudziakpudziak <lukaszdudziak777@gmail.com>
2021-02-24 21:33:57 +0100

    modalsDone+sliderStart

--- rytmy.pl/script.js
+++ rytmy.pl/script.js
@@ -1,4 +1,6 @@
-const slider = document.getElementById("slider-div");
+//modal
+
+const modalWrapper = document.getElementById("modal-wrapper");
 
 const musicButton = document.getElementById("music-button");
 const musicModal = document.getElementById("music-modal");
@@ -9,27 +11,36 @@ const streetModal = document.getElementById("street-modal");
 const hotOnesButton = document.getElementById("hotOnes-button");
 const hotOnesModal = document.getElementById("hotOnes-modal");
 
+function showCoords(event) {
+  let y = event.clientY;
+  if (y > 530) {
+    clearModals();
+  }
+}
+
 musicButton.addEventListener("mouseover", () => {
   clearModals();
+  modalWrapper.classList.remove("hidden");
   musicModal.classList.remove("hidden");
 });
 
 streetButton.addEventListener("mouseover", () => {
   clearModals();
+  modalWrapper.classList.remove("hidden");
   streetModal.classList.remove("hidden");
 });
 
 hotOnesButton.addEventListener("mouseover", () => {
   clearModals();
+  modalWrapper.classList.remove("hidden");
   hotOnesModal.classList.remove("hidden");
 });
 
 clearModals = () => {
+  modalWrapper.classList.add("hidden");
   musicModal.classList.add("hidden");
   streetModal.classList.add("hidden");
   hotOnesModal.classList.add("hidden");
 };
 
-slider.addEventListener("mouseover", clearModals);
-
-//modal wrapper - to make white bgc + clearing modals
+//slider
