bcdb54f2a244b48e58083cf4544e1d53b779319e
dudziakpudziak <lukaszdudziak777@gmail.com>
2021-08-03 15:56:37 +0200

    Add navigation js

--- SingleElements/slider/main.js
+++ SingleElements/slider/main.js
@@ -1,1 +1,64 @@
+const names = [
+  "Jarabacoa Organic",
+  "Locally Roasted Coffee",
+  "Santa Clara Lavado",
+];
 
+const descriptions = [
+  "Medium roasted whole beans allowing the true flavor characteristic to come through for a remarkable taste.",
+  "It's just coffee. Nothing special.",
+  "THIS BLEND DANCES PERFECTLY WITH A FILTER. YET, IT IS JUST AS FABULOUS FOR ALL OTHER BREW METHODS. ",
+];
+
+const prices = ["16$", "5$", "99$"];
+
+const images = ["./img/1.png", "./img/2.png", "./img/3.png"];
+
+const sliderElement = document.getElementById("js-slider-element");
+const coffeeName = document.getElementById("js-element-name");
+const coffeeDescription = document.getElementById("js-element-description");
+const coffeeImage = document.getElementById("js-element-image");
+const previousCoffee = document.getElementById("js-previous");
+const nextCoffee = document.getElementById("js-next");
+const buyCoffee = document.getElementById("js-buy");
+
+let coffeeNumber = 0;
+let changeDescriptionsTimer;
+
+const coffeeChange = () => {
+  coffeeName.innerHTML = names[coffeeNumber];
+  coffeeDescription.innerHTML = descriptions[coffeeNumber];
+  coffeeImage.src = images[coffeeNumber];
+  buyCoffee.innerHTML = `Buy \(${prices[coffeeNumber]}\)`;
+  setTimeout(
+    () =>
+      sliderElement.classList.remove(
+        "slider-element-animation",
+        "slider-element-animation-reverse"
+      ),
+
+    1000
+  );
+};
+
+const navigationHandler = (e) => {
+  clearTimeout(changeDescriptionsTimer);
+  if (e.target.innerHTML === "Next") {
+    coffeeNumber++;
+    if (coffeeNumber >= names.length) {
+      coffeeNumber = 0;
+    }
+    sliderElement.classList.add("slider-element-animation");
+  }
+  if (e.target.innerHTML === "Previous") {
+    coffeeNumber--;
+    if (coffeeNumber < 0) {
+      coffeeNumber = names.length - 1;
+    }
+    sliderElement.classList.add("slider-element-animation-reverse");
+  }
+  changeDescriptionsTimer = setTimeout(coffeeChange, 1000);
+};
+
+nextCoffee.addEventListener("click", navigationHandler);
+previousCoffee.addEventListener("click", navigationHandler);
